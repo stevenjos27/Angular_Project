0fe28339682b199bc31f08d5b3c282037ab95704
stevenjos27 <stevenj27@outlook.com>
2018-09-09 15:38:27 +0530

    Add Comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import 'hammerjs';
 
@@ -64,7 +65,8 @@ import { LoginComponent } from './login/login.component';
     MatSelectModule,
     MatSlideToggleModule,
     ReactiveFormsModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [ LoginComponent ],
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -55,20 +56,45 @@ const DISH = {
 })
 export class DishdetailComponent implements OnInit {
 
+    @ViewChild('fform') commentFormDirective;
+
+    commentForm: FormGroup;
+    newComment: any;
+
+    formErrors = {
+        'author': '',
+        'rating': '',
+        'comment': ''
+    };
+
+    validationMessages = {
+        'author': {
+            'required': 'Author Name is required.',
+            'minlength': 'Author Name must be at least 2 characters long.'// ,
+            // 'maxlength': 'Name cannot be more than 25 characters long.'
+        },
+        'comment': {
+            'required': 'Comment is required.',
+            'minlength': 'Comment must be at least 2 characters long.',
+        }
+    };
+
     dish: Dish;
     dishIds: number[];
     prev: number;
     next: number;
 
     constructor(private dishservice: DishService, private route: ActivatedRoute,
-        private location: Location) { }
+        private location: Location, private fb: FormBuilder) {
+        this.createForm();
+    }
 
     ngOnInit() {
         this.dishservice.getDishIds().subscribe(dishIds =>
             this.dishIds = dishIds);
         this.route.params.pipe(switchMap((params: Params) =>
             this.dishservice.getDish(+params['id'])))
-        .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+            .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     }
 
     setPrevNext(dishId: number) {
@@ -81,6 +107,62 @@ export class DishdetailComponent implements OnInit {
 
     goBack(): void {
         this.location.back();
+    }
+
+    createForm(): void {
+        this.commentForm = this.fb.group({
+            author: ['', [Validators.required, Validators.minLength(2)]], // , Validators.maxLength(25)]],
+            rating: ['5'],
+            comment: ['', Validators.minLength(2)]
+        });
+
+        this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+        this.onValueChanged();
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.commentForm) { return; }
+
+        const form = this.commentForm;
+
+        for (const field in this.formErrors) {
+            if (this.formErrors.hasOwnProperty(field)) {
+                // clear previous error message (if any)
+                this.formErrors[field] = '';
+                const control = form.get(field);
+                if (control && control.dirty && !control.valid) {
+                    const messages = this.validationMessages[field];
+                    for (const key in control.errors) {
+                        if (control.errors.hasOwnProperty(key)) {
+                            this.formErrors[field] += messages[key] + ' ';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    onSubmit() {
+        this.newComment = this.commentForm.value;
+
+        const date = new Date().toISOString();
+
+        console.log(this.newComment);
+        this.dish.comments.push({
+            rating: this.newComment.rating,
+            comment: this.newComment.comment,
+            author: this.newComment.author,
+            date: date
+        });
+        this.commentFormDirective.resetForm();
+
+        this.commentForm.reset({
+            author: '',
+            rating: '5',
+            comment: ''
+        });
+
     }
 
 }
